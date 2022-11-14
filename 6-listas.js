671cb9073dc513dcb3619a14d082edc4117c3d54
renatopl97 <renatopl97@gmail.com>
2022-11-14 19:27:43 -0300

    identação

--- 6-listas.js
+++ 6-listas.js
@@ -6,16 +6,12 @@ console.log(`Trabalhando com listas`);
 //para comentar código, seleciona o que for comentar e usa ctrl+k+c
 //para retirar comentário ctrl+k+u
 
-const listaDeDestinos = new Array(
-    `Salvador`,
-    `São Paulo`,
-    `Rio de Janeiro`
-);
+const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
 
-listaDeDestinos.push(`Curitiba`);//para adicionar uma informação ao array
+listaDeDestinos.push(`Curitiba`); //para adicionar uma informação ao array
 console.log(`Destinos possíveis`);
 console.log(listaDeDestinos);
 
-listaDeDestinos.splice(1,1);//retirar um elemento do array, primeiro espaço é a posição do elemento e o segundo é quantos elementos retirar (no js, a contagem começa em 0)
+listaDeDestinos.splice(1, 1); //retirar um elemento do array, primeiro espaço é a posição do elemento e o segundo é quantos elementos retirar (no js, a contagem começa em 0)
 console.log(listaDeDestinos);
-console.log(listaDeDestinos[1]);//colocando a posição de um item entre colchetes é possível imprimir apenas um determinado item
+console.log(listaDeDestinos[1]); //colocando a posição de um item entre colchetes é possível imprimir apenas um determinado item
--- 7-condicionais.js
+++ 7-condicionais.js
@@ -1,28 +1,24 @@
 console.log(`Trabalhando com condicionais`);
 
-const listaDeDestinos = new Array(
-    `Salvador`,
-    `São Paulo`,
-    `Rio de Janeiro`
-);
+const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
 
 const idadeComprador = 15;
-const estaAcompanhado = false; //se verdadeiro=acompanhado e se false=sem companhia 
+const estaAcompanhado = false; //se verdadeiro=acompanhado e se false=sem companhia
 
 console.log(`Destinos possíveis`);
 console.log(listaDeDestinos);
 //A estrutura começa vendo SE o comprador é maior, se sim, diz que é maior e remove a cidade selecionada, senão, entra nas possibilidades de menor
 //Se acompanhado pode comprar passagem, senão retornar que é menor e desacompanhado, não pode comprar
 if (idadeComprador >= 18) {
-    console.log("Comprador maior de idade.");
-    listaDeDestinos.splice(1,1);
-}else{
-    if (estaAcompanhado){
-        console.log("Comprador menor de idade, mas acompanhado.");
-        listaDeDestinos.splice(1,1);
-    }else{
-        console.log("Comprador menor de idade e desacompanhado, não posso vender.");
-    }
+  console.log("Comprador maior de idade.");
+  listaDeDestinos.splice(1, 1);
+} else if (estaAcompanhado) {
+  console.log("Comprador menor de idade, mas acompanhado.");
+  listaDeDestinos.splice(1, 1);
+} else {
+  console.log("Comprador menor de idade e desacompanhado, não posso vender.");
 }
 
 console.log(listaDeDestinos);
+
+//para "identar" o código alt+shift+f
